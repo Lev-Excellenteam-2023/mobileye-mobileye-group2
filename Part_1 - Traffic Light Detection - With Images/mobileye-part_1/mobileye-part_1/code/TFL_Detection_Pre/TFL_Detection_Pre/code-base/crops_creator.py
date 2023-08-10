cb5b2182704bf79049838d3241e49c5eb0cef770
Daniel Bodenstein <danielboden111@gmail.com>
2023-08-10 13:42:46 +0300

    make_crops

--- Part_1 - Traffic Light Detection - With Images/mobileye-part_1/mobileye-part_1/code/TFL_Detection_Pre/TFL_Detection_Pre/code-base/crops_creator.py
+++ Part_1 - Traffic Light Detection - With Images/mobileye-part_1/mobileye-part_1/code/TFL_Detection_Pre/TFL_Detection_Pre/code-base/crops_creator.py
@@ -1,11 +1,14 @@
 from typing import Dict, Any
 
 from consts import CROP_DIR, CROP_RESULT, SEQ, IS_TRUE, IGNOR, CROP_PATH, X0, X1, Y0, Y1, COLOR, SEQ_IMAG, COL, X, Y, \
-    GTIM_PATH
+    GTIM_PATH, IMAG_PATH
 
 from pandas import DataFrame
 import cv2
 import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
 
 def make_big_crop(image, x, y, color):
@@ -17,16 +20,18 @@ def make_big_crop(image, x, y, color):
         patch_top_left_x = x - 20
         patch_top_left_y = y - 20
     else:
-        patch_top_left_x = x - 20
+        patch_top_left_x = x - 20 if x - 20 > 0 else 0
         patch_top_left_y = y - 140 if y - 140 > 0 else 0
+    patch_top_right_x = patch_top_left_x + patch_width if patch_top_left_x + patch_width < image.shape[1] else image.shape[1]
+    patch_top_right_y = patch_top_left_y + patch_height if patch_top_left_y + patch_height < image.shape[0] else image.shape[0]
 
-    big_crop = image[patch_top_left_y:patch_top_left_y + patch_height,
-                      patch_top_left_x:patch_top_left_x + patch_width, :]
+    big_crop = image[patch_top_left_y:patch_top_right_y,
+                      patch_top_left_x:patch_top_right_x, :]
 
     return big_crop
 
 
-def make_crop(image, x, y, color):
+def make_crop(image_path, x, y, color):
     """
     The function that creates the crops from the image.
     Your return values from here should be the coordinates of the crops in this format (x0, x1, y0, y1, crop content):
@@ -35,13 +40,16 @@ def make_crop(image, x, y, color):
     'y0'  The smaller y value (the lower corner)
     'y1'  The bigger y value (the higher corner)
     """
+    # using pillow to load the image
+    image: Image = Image.open(image_path)
+    # converting the image to a numpy ndarray array
+    c_image: np.ndarray = np.array(image)
 
-
-    big_crop = make_big_crop(image, x, y, color)
-    image = cv2.cvtColor(big_crop, cv2.COLOR_RGB2BGR)
+    big_crop = make_big_crop(c_image, x, y, color)
+    image1 = cv2.cvtColor(big_crop, cv2.COLOR_RGB2BGR)
 
     # Convert the image to grayscale for edge detection
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
 
     # Apply Gaussian blur to reduce noise
     blurred = cv2.GaussianBlur(gray, (5, 5), 0)
@@ -52,19 +60,23 @@ def make_crop(image, x, y, color):
     # Apply Hough Circle Transform
     circles = cv2.HoughCircles(edges, cv2.HOUGH_GRADIENT, dp=1, minDist=50, param1=100, param2=30, minRadius=10,
                                maxRadius=50)
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        circle = circles[0][0]
+        diameter = circle[2] * 2
+        crop_width = int(diameter * 1.4)
+        crop_height = crop_width * 3
 
-    diameter = circles[0][2] * 2
-    crop_width = int(diameter * 1.4)
-    crop_height = crop_width * 3
+        if color == 'r':
+            crop_top_left_x = x - 20
+            crop_top_left_y = y - 20
+        else:
+            crop_top_left_x = x - 20
+            crop_top_left_y = y - crop_width * 2 if y - crop_width * 2 > 0 else 0  # if the tfl go up of the image: y=0
 
-    if color == 'r':
-        crop_top_left_x = x - 20
-        crop_top_left_y = y - 20
+        return crop_top_left_x + crop_width, crop_top_left_x, crop_top_left_y, crop_top_left_y + crop_height, 'crop_data'
     else:
-        crop_top_left_x = x - 20
-        crop_top_left_y = y - crop_width * 2 if y - crop_width * 2 > 0 else 0  # if the tfl go up of the image: y=0
-
-    return crop_top_left_x, crop_top_left_x + crop_width, crop_top_left_y, crop_top_left_y + crop_height, 'crop_data'
+        return x + 13, x - 13, y + 52, y - 26, 'crop'
 
 
 def check_crop(*args, **kwargs):
@@ -73,6 +85,7 @@ def check_crop(*args, **kwargs):
     Try using the ground truth to do that (Hint: easier than you think for the simple cases, and if you found a hard
     one, just ignore it for now :). )
     """
+
     return True, True
 
 
@@ -106,16 +119,35 @@ def create_crops(df: DataFrame) -> DataFrame:
         # example code:
         # ******* rewrite ONLY FROM HERE *******
 
-        x0, x1, y0, y1, crop = make_crop(df[X], df[Y], 'and_everything_else_you_need_here (and you need)')
-        result_template[X0], result_template[X1], result_template[Y0], result_template[Y1] = x0, x1, y0, y1
-        crop_path: str = '/data/crops/my_crop_unique_name.probably_containing_the original_image_name+somthing_unique'
-        # crop.save(CROP_DIR / crop_path)
-        result_template[CROP_PATH] = crop_path
-        result_template[IS_TRUE], result_template[IGNOR] = check_crop(df[GTIM_PATH],
-                                                                      crop,
-                                                                      'everything_else_you_need_here')
-        # ******* TO HERE *******
+        x0, x1, y0, y1, crop = make_crop(df[IMAG_PATH][index], df[X][index], df[Y][index], df[COLOR][index])
 
-        # added to current row to the result DataFrame that will serve you as the input to part 2 B).
-        result_df = result_df._append(result_template, ignore_index=True)
+        fig, ax = plt.subplots()
+        image = Image.open(df[IMAG_PATH][index])
+        c_image = np.array(image)
+        ax.imshow(c_image)
+
+        # Create a rectangle patch
+        if (x0):
+            rect = patches.Rectangle((x1, y0), x0 - x1, y1 - y0,
+                                     linewidth=2, edgecolor='r', facecolor='none')
+
+            # Add the rectangle to the plot
+            ax.add_patch(rect)
+
+            # Show the plot
+            plt.show()
+
+
+            result_template[X0], result_template[X1], result_template[Y0], result_template[Y1] = x0, x1, y0, y1
+            crop_path: str = '/data/crops/my_crop_unique_name.probably_containing_the original_image_name+somthing_unique'
+            # crop.save(CROP_DIR / crop_path)
+            result_template[CROP_PATH] = crop_path
+            result_template[IS_TRUE], result_template[IGNOR] = check_crop(df[GTIM_PATH],
+                                                                        crop,
+                                                                        'everything_else_you_need_here')
+            # ******* TO HERE *******
+
+            # added to current row to the result DataFrame that will serve you as the input to part 2 B).
+            result_df = result_df._append(result_template, ignore_index=True)
+
     return result_df
